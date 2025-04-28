804aaf6abc96652c419b30d1f7e5d244e72f56fb
Matheus Szoke <matheusszoke@gmail.com>
2025-04-28 18:30:48 -0300

    first commit

---
+++ ED/Controllers/PlatformController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ED.Controllers
+{
+    public class PlatformController : Controller
+    {
+        // GET: Platform
+        public ActionResult Index_v2()
+        {
+            return View();
+        }
+    }
+}
